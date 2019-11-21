e38fb69113ffebe209cdb69c55dc7baf3fed7c8e
Sumit Ghewade <sumit.ghewade@sap.com>
2019-11-21 10:24:13 -0800

    Updates

---
+++ precache-manifest.40b394902f24d50a6bd490eb853aecef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e09e3f72a2f283530a87ebc360bfe42a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "befe9059977beb63dd55",
+    "url": "/monsters-rolodex/static/css/main.40a2d153.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "befe9059977beb63dd55",
+    "url": "/monsters-rolodex/static/js/main.1d0b710d.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
