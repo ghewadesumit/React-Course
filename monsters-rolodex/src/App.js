e0e358d6a6d44bd766b4571f69f499e067b1d5ea
Sumit Ghewade <sumit.ghewade@sap.com>
2019-11-21 10:56:18 -0800

    fixing css

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -27,7 +27,7 @@ class App extends Component {
     console.log("Filtered Monsters are", filteredMonster);
     return (
       <div className="App">
-        <h1> Monster Rolodex</h1>
+        <h1 className="main-header"> Monster Rolodex</h1>
         <SearchBox
           placeholder="Search Monsters"
           handleChange={this.handleChange}
